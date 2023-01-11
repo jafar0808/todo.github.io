360d84eb0bd346a4402f4792a5a86b6c5b0cbfbb
jafar sadik <m.jafarsk@gmail.com>
2023-01-11 00:39:29 -0800

    initial stage

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Chart from './Chart';
+import Dashboard from './Dashboard';
+
+function App() {
+  return (
+    <div className="App">
+      
+        <Dashboard/>
+      
+    </div>
+  );
+}
+
+export default App;
